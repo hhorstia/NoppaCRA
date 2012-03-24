4a5cac6009daea58af3e67871f8cbec916b13982
Rami Alatalo <rami.alatalo@gmail.com>
2012-03-24 21:05:48 +0200

    Additions for the HTTP API.

--- src/api/index.php
+++ src/api/index.php
@@ -1,7 +1,13 @@
 <?php
 
+//error_reporting(E_ALL);
+//ini_set('display_errors', '1');
+
+require_once('TestDatabase.php');
+require_once('TestUser.php');
 require_once('HttpApi.php');
 
+NoppaCRA\TestDatabase::init();
 exit(NoppaCRA\HttpApi::resolve($_POST["method"]));
 
 ?>
