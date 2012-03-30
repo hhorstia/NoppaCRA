04a94718ff6a2e344b1afa73b632820f35bf7ba5
Rami Alatalo <rami.alatalo@gmail.com>
2012-03-30 15:40:17 +0300

    Fixes the HttpApi file.

--- src/api/HttpApi.php
+++ src/api/HttpApi.php
@@ -54,24 +54,68 @@ Class HttpApi {
 	}
 	
 	public static function resolve($method) {
-		$response = array("method" => "", "valid" => 0);
+		$response = array('method' => '', 'valid' => 0);
 		
 		if (isset($method)) {
 			
-			$response["method"] = $method;
-			$response["valid"] = 1;
+			$response['method'] = $method;
+			$response['valid'] = 1;
 			
 			switch ($method) {
-				case "getCourseRating":
-					if (isset($_POST["id"])) {
-						$response["id"] = $_POST["id"];
-						$response["value"] = HttpApi::getCourseRating($_POST["id"]);
+				case 'getCourse':
+					if (isset($_POST['id'])) {
+						$response['id'] = $_POST['id'];
+						$response['value'] = HttpApi::getCourse($response['id']);
 					} else {
-						$response["valid"] = 0;
+						$response['valid'] = 0;
+					}
+					break;
+				case 'getCourses':
+					$response['sort'] = $_POST['sort'] ? $_POST['sort'] : 'code';
+					$response['limit'] = $_POST['limit'] ? $_POST['limit'] : 10;
+					$response['offset'] = $_POST['offset'] ? $_POST['offset'] : 0;
+					$response['value'] = HttpApi::getCourses($response['sort'], $response['limit'], $response['offset']);
+					break;
+				case 'getUserCourses':
+					if (TestUser::isAuthenticated()) {
+						$response['active'] = $_POST['active'] ? $_POST['active'] : 0;
+						$response['value'] = HttpApi::getUserCourses($response['active']);
+					} else {
+						$response['valid'] = 0;
+					}
+					break;
+				case 'getCourseRecommendations':
+					$response['sort'] = $_POST['sort'] ? $_POST['sort'] : 'code';
+					$response['limit'] = $_POST['limit'] ? $_POST['limit'] : 10;
+					$response['offset'] = $_POST['offset'] ? $_POST['offset'] : 0;
+					$response['value'] = HttpApi::getCourseRecommendations($response['sort'], $response['limit'], $response['offset'], $response['filters']);
+					break;
+				case 'getCourseReviews':
+					if (isset($_POST['id'])) {
+						$response['id'] = $_POST['id'];
+						$response['value'] = HttpApi::getCourseReviews($response['id']);
+					} else {
+						$response['valid'] = 0;
+					}
+					break;
+				case 'getUserCourseReview':
+					if (isset($_POST['id'])) {
+						$response['id'] = $_POST['id'];
+						$response['value'] = HttpApi::getUserCourseReview($response['id']);
+					} else {
+						$response['valid'] = 0;
+					}
+					break;
+				case 'getCourseRating':
+					if (isset($_POST['id'])) {
+						$response['id'] = $_POST['id'];
+						$response['value'] = HttpApi::getCourseRating($response['id']);
+					} else {
+						$response['valid'] = 0;
 					}
 					break;
 				default:
-					$response["valid"] = 0;
+					$response['valid'] = 0;
 			}
 		}
 		
