be6e578c68cf15069f17ad739c49863f365a2e75
Rami Alatalo <rami.alatalo@gmail.com>
2012-03-30 15:57:06 +0300

    One commit to rule them all.

---
+++ src/api/TestUser.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace NoppaCRA;
+
+Class TestUser {
+
+	public static function isAuthenticated() {
+		return true;
+	}
+	
+	public static function getID() {
+		return 1;
+	}
+
+}
+
+?>
+		
